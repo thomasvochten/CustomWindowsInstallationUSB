e5952029d1e486e3e4998dd0ff61bfe4caadcb87
Thomas Vochten <mail@thomasvochten.com>
2023-12-19 11:03:54 +0100

    Custom parameter change

--- Commands.ps1
+++ Commands.ps1
@@ -1,13 +1,14 @@
-Import-Module C:\Dev\CustomWindowsInstallationUSB -Force -Verbose
+Import-Module C:\Users\ThomasVochten\dev\CustomWindowsInstallationUSB -Force -Verbose
 
 $newCustomWindowsInstallationUSBSplat = @{
-    DriverPath = 'C:\Dev\WinInstall\Drivers'
+    DriverPath = 'C:\Users\ThomasVochten\dev\WinInstall\Drivers'
     WindowsVersion = "Windows 11 Enterprise"
-    UpdatesPath = 'C:\Dev\WinInstall\Updates'
+    UpdatesPath = 'C:\Users\ThomasVochten\dev\WinInstall\Updates'
     USBDriveLetter = 'D'
-    ExtraPath = 'C:\Dev\WinInstall\Extra'
-    ISOFile        = 'C:\Dev\WinInstall\ISO\en-us_windows_11_business_editions_version_23h2_x64_dvd_a9092734.iso'
+    ExtraPath = 'C:\Users\ThomasVochten\dev\WinInstall\Extra'
+    ISOFile        = 'C:\Users\ThomasVochten\dev\WinInstall\ISO\en-us_windows_11_business_editions_version_23h2_x64_dvd_a9092734.iso'
     USBDriveLabel = 'WIN11'
+    WorkingDirectory = 'C:\Users\ThomasVochten\dev\temp'
 }
 
 New-CustomWindowsInstallationUSB @newCustomWindowsInstallationUSBSplat
