6ab8617123e8e2bb4711bd80ab8d64359f9f5f0a
Thomas Vochten <mail@thomasvochten.com>
2024-10-03 14:23:29 +0200

    Temp folder test and creation logic

--- Commands.ps1
+++ Commands.ps1
@@ -6,9 +6,13 @@ $newCustomWindowsInstallationUSBSplat = @{
     UpdatesPath = 'C:\Users\ThomasVochten\dev\WinInstall\Updates'
     USBDriveLetter = 'D'
     ExtraPath = 'C:\Users\ThomasVochten\dev\WinInstall\Extra'
-    ISOFile        = 'C:\Users\ThomasVochten\dev\WinInstall\ISO\en-us_windows_11_business_editions_version_23h2_x64_dvd_a9092734.iso'
+    ISOFile        = 'C:\Users\ThomasVochten\dev\WinInstall\ISO\en-us_windows_11_business_editions_version_24h2_x64_dvd_59a1851e.iso'
     USBDriveLabel = 'WIN11'
     WorkingDirectory = 'C:\Users\ThomasVochten\dev\temp'
+}
+
+if (-not (Test-Path $newCustomWindowsInstallationUSBSplat.WorkingDirectory)) {
+    New-Item -Path $newCustomWindowsInstallationUSBSplat.WorkingDirectory -ItemType Directory
 }
 
 New-CustomWindowsInstallationUSB @newCustomWindowsInstallationUSBSplat
