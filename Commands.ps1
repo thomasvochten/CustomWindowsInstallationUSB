4c32c40df3a76e3f31b91f3a6047d7b81197f15a
Thomas Vochten <mail@thomasvochten.com>
2025-09-19 06:47:57 +0200

    Win 11 Pro

--- Commands.ps1
+++ Commands.ps1
@@ -2,11 +2,11 @@ Import-Module C:\Users\ThomasVochten\dev\CustomWindowsInstallationUSB -Force -Ve
 
 $newCustomWindowsInstallationUSBSplat = @{
     DriverPath = 'C:\Users\ThomasVochten\dev\WinInstall\Drivers'
-    WindowsVersion = "Windows 11 Enterprise"
+    WindowsVersion = "Windows 11 Pro"
     UpdatesPath = 'C:\Users\ThomasVochten\dev\WinInstall\Updates'
     USBDriveLetter = 'D'
     ExtraPath = 'C:\Users\ThomasVochten\dev\WinInstall\Extra'
-    ISOFile        = 'C:\Users\ThomasVochten\dev\WinInstall\ISO\en-us_windows_11_business_editions_version_24h2_x64_dvd_59a1851e.iso'
+    ISOFile        = 'C:\Users\ThomasVochten\dev\WinInstall\ISO\en-gb_windows_11_business_editions_version_24h2_updated_sep_2025_x64_dvd_3bb1c3a0.iso'
     USBDriveLabel = 'WIN11'
     WorkingDirectory = 'C:\Users\ThomasVochten\dev\temp'
 }
